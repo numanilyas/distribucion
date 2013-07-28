09ec18677e5a0c4f43e7030f7a195ff92654c1e5
Numan Ilyas <numan.ilyas@gmail.com>
2013-07-28 16:44:43 -0400

    updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "Home of Embroibered Products - Distribucion Es Guadalquivir"
+    base_title = "Home of Embroibered Products - Distribuciones Guadalquivir"
     if page_title.empty?
       base_title
     else
-      "#{page_title} - Distribucion Es Guadalquivir"
+      "#{page_title} - Distribuciones Guadalquivir"
     end    
   end
   
